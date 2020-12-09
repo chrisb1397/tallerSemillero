d0ab3ddb090fda21eade33d171441e0d3db5d19b
ChristianB <christian.benitez@epn.edu.ec>
2020-12-09 12:41:10 -0500

    Actualizacion de registros en Estudiante

---
+++ src/main/java/com/clearminds/cbe/bdd/test/TestServicioActualizar.java
@@ -0,0 +1,20 @@
+package com.clearminds.cbe.bdd.test;
+
+import com.clearminds.cbe.dtos.Estudiante;
+import com.clearminds.cbe.excepciones.BDDException;
+import com.clearminds.cbe.servicios.ServicioEstudiante;
+
+public class TestServicioActualizar {
+
+	public static void main(String[] args) {
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.actualizarEstudiante(new Estudiante(2, "Pedro", "Navaja"));
+		} catch (BDDException e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+
+	}
+
+}
--- src/main/java/com/clearminds/cbe/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/cbe/servicios/ServicioEstudiante.java
@@ -16,17 +16,38 @@ public class ServicioEstudiante extends ServicioBase {
 		srvb.abrirConexion();
 		System.out.println("Insertando estudiante: " + estudiante);
 		Statement stmt = null;
-		
+
 		try {
 			stmt = con.createStatement();
-			String sql = "insert into estudiantes(nombre,apellido)values('"+estudiante.getNombre()+"','"+ estudiante.getApellido()+"')";
-			System.out.println("Script: "+sql);
+			String sql = "insert into estudiantes(nombre,apellido)values('" + estudiante.getNombre() + "','"
+					+ estudiante.getApellido() + "')";
+			System.out.println("Script: " + sql);
 			stmt.execute(sql);
 			srvb.cerrarConexion();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new BDDException("Error al insertar estudiante");
 		}
-		
+
+	}
+
+	public void actualizarEstudiante(Estudiante estudiante) throws BDDException {
+		Connection con = ConexionBDD.obtenerConexion();
+		ServicioBase srvb = new ServicioBase();
+		srvb.abrirConexion();
+		System.out.println("Actualizando estudiante: " + estudiante);
+		Statement stmt = null;
+
+		try {
+			stmt = con.createStatement();
+			String sql = "update estudiantes set nombre='"+estudiante.getNombre()+"', apellido='"+estudiante.getApellido()+"' where id="+estudiante.getId();
+			System.out.println("Script: " + sql);
+			stmt.execute(sql);
+			srvb.cerrarConexion();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new BDDException("Error al actualizar estudiante");
+		}
+
 	}
 }
