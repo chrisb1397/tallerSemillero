85e614a6e1ffc38f8006e4f01118fe3a6591da83
ChristianB <christian.benitez@epn.edu.ec>
2020-12-09 12:25:19 -0500

    Inserción de datos a SQL desde métodos en ServicioEstudiante

--- src/main/java/com/clearminds/cbe/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/cbe/servicios/ServicioEstudiante.java
@@ -1,14 +1,32 @@
 package com.clearminds.cbe.servicios;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.clearminds.cbe.bdd.ConexionBDD;
 import com.clearminds.cbe.dtos.Estudiante;
 import com.clearminds.cbe.excepciones.BDDException;
 
-public class ServicioEstudiante extends ServicioBase{
+public class ServicioEstudiante extends ServicioBase {
 
-	public void insertarEstudiante(Estudiante estudiante) throws BDDException{
+	public void insertarEstudiante(Estudiante estudiante) throws BDDException {
+		Connection con = ConexionBDD.obtenerConexion();
 		ServicioBase srvb = new ServicioBase();
-			srvb.abrirConexion();
-			System.out.println("Insertando estudiante: " + estudiante);
+		srvb.abrirConexion();
+		System.out.println("Insertando estudiante: " + estudiante);
+		Statement stmt = null;
+		
+		try {
+			stmt = con.createStatement();
+			String sql = "insert into estudiantes(nombre,apellido)values('"+estudiante.getNombre()+"','"+ estudiante.getApellido()+"')";
+			System.out.println("Script: "+sql);
+			stmt.execute(sql);
 			srvb.cerrarConexion();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new BDDException("Error al insertar estudiante");
+		}
+		
 	}
 }
